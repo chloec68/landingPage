ec80ffd3856ccef4a8cdec7bff5d87eb40d3ebf2
Chloé Cilia <chloe.cilia68@gmail.com>
2024-11-23 17:32:36 +0100

    darkMode starters

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ function burgerMenuMobile(){
         menuItems.style.display = "block";
     }
 }
+
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+    const darkModeButton = document.getElementById("darkModeButton");
+
+    if(darkModeButton.innerHTML === "DARK MODE"){
+        darkModeButton.innerHTML = "LIGHT MODE";
+        darkModeButton.style.backgroundColor = "#ffffff";
+        darkModeButton.style.color="#000000";
+    }else{
+        darkModeButton.innerHTML = "DARK MODE"
+        darkModeButton.style.backgroundColor = "#000000";
+        darkModeButton.style.color="#ffffff";
+    }
+ }
